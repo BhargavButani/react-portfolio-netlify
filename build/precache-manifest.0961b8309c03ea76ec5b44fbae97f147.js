0208c2555c447bc60fe40ca826850c1faa431146
Bhargav Butani <sinubutani@gmail.com>
2020-09-12 19:21:44 +0530

    final edit

---
+++ build/precache-manifest.0961b8309c03ea76ec5b44fbae97f147.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bab3248c83cc446e91d5cceccabd615",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbdceb09c35c1fd0088c",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbdceb09c35c1fd0088c",
+    "url": "/static/js/main.4eb856d1.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
